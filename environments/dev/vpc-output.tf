d3ba6daea738421554d32394b249de5633d25c40
KuzeGY <30339590+KuzeGY@users.noreply.github.com>
2023-06-21 00:38:11 +1000

    test github action with terraform cloud, #21

--- environments/dev/vpc-output.tf
+++ environments/dev/vpc-output.tf
@@ -1,37 +1,37 @@
-# # Outputs for VPC
+# Outputs for VPC
 
-# # VPC ID
-# output "vpc_id" {
-#   description = "The ID of the VPC"
-#   value       = module.vpc.vpc_id
-# }
+# VPC ID
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = module.vpc.vpc_id
+}
 
-# # VPC CIDR blocks
-# output "vpc_cidr_block" {
-#   description = "The CIDR block of the VPC"
-#   value       = module.vpc.vpc_cidr_block
-# }
+# VPC CIDR blocks
+output "vpc_cidr_block" {
+  description = "The CIDR block of the VPC"
+  value       = module.vpc.vpc_cidr_block
+}
 
-# # VPC Private Subnets
-# output "private_subnets" {
-#   description = "List of IDs of private subnets"
-#   value       = module.vpc.private_subnets
-# }
+# VPC Private Subnets
+output "private_subnets" {
+  description = "List of IDs of private subnets"
+  value       = module.vpc.private_subnets
+}
 
-# # VPC Public Subnets
-# output "public_subnets" {
-#   description = "List of IDs of public subnets"
-#   value       = module.vpc.public_subnets
-# }
+# VPC Public Subnets
+output "public_subnets" {
+  description = "List of IDs of public subnets"
+  value       = module.vpc.public_subnets
+}
 
-# # VPC NAT gateway Public IP
-# output "nat_public_ips" {
-#   description = "List of public Elastic IPs created for AWS NAT Gateway"
-#   value       = module.vpc.nat_public_ips
-# }
+# VPC NAT gateway Public IP
+output "nat_public_ips" {
+  description = "List of public Elastic IPs created for AWS NAT Gateway"
+  value       = module.vpc.nat_public_ips
+}
 
-# # VPC ARN
-# output "vpc_arn" {
-#   description = "VPC ARN"
-#   value       = module.vpc.vpc_arn
-# }
+# VPC ARN
+output "vpc_arn" {
+  description = "VPC ARN"
+  value       = module.vpc.vpc_arn
+}
